66871fcf81450976ee47c58a7885bffed3252449
Anna-Korchahina <korchagina.anna.v@gmail.com>
2024-01-07 15:13:29 +0200

    feat: do task calculateBalance

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -357,8 +357,14 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  return arr.reduce((sum, elem) => {
+    return sum + elem[0] - elem[1];
+  }, 0);
 }
 
 /**
