7cdfa8961c1b430de8d05facb65b9138d81a4343
Anna-Korchahina <korchagina.anna.v@gmail.com>
2024-01-07 10:48:10 +0200

    feat: do task isValueEqualsIndex

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -184,8 +184,14 @@ function isSameLength(arr) {
  *    isValueEqualsIndex([2, 1, 0, 4, 5]) => true
  *    isValueEqualsIndex([10, 20, 30, 40, 50]) => false
  */
-function isValueEqualsIndex(/* arr */) {
-  throw new Error('Not implemented');
+function isValueEqualsIndex(arr) {
+  return arr.some((elem, index) => {
+    if (elem === index) {
+      return true;
+    }
+
+    return false;
+  });
 }
 
 /**
