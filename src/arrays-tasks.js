f9583fd950233e9127a04217566f37583b540421
Anna-Korchahina <korchagina.anna.v@gmail.com>
2024-01-07 15:35:30 +0200

    feat: do task getFalsyValuesCount

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -435,8 +435,13 @@ function getElementByIndices(/* arr, indices */) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.reduce((sum, elem) => {
+    if (elem) {
+      return sum;
+    }
+    return sum + 1;
+  }, 0);
 }
 
 /**
